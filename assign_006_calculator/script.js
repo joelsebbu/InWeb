45c9170cc7219f6ad89fd9e836905bc5add61714
joelsebbu <joel18sebastian@gmail.com>
2022-08-10 12:54:33 +0530

    UI done

---
+++ assign_006_calculator/script.js
@@ -0,0 +1,9 @@
+ac= document.getElementById("AC");
+// ac.addEventListener("click",function(){
+    
+//     var bgColor =ac.style.backgroundColor;
+//     var color =ac.style.color;
+//     document.getElementById("AC").style.backgroundColor = "orange";
+//     document.getElementById("AC").style.color="#191b1b";
+
+// });
