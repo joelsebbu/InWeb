582fd43fc80b4c49d97340a74844cbdaf81047f8
joelsebbu <joel18sebastian@gmail.com>
2022-08-16 13:11:14 +0530

    grid

--- camp_eval/script.js
+++ camp_eval/script.js
@@ -2,3 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelector('.sidenav');
     var instances = M.Sidenav.init(elems);
 });
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelector('.right hide-on-med-and-down');
+//     var instances = M.Sidenav.init(elems);
+// });
