f7cc3b4ba8088afa84922358464d7f1e4351172a
joelsebbu <joel18sebastian@gmail.com>
2022-08-22 15:41:26 +0530

    REST CRUD works

--- BackEnd/flask_001/app.py
+++ BackEnd/flask_001/app.py
@@ -1,4 +1,6 @@
+import re
 from flask import Flask,redirect,url_for,request,jsonify,abort
+import requests
 
 app= Flask(__name__)
 
@@ -47,7 +49,7 @@ booklist=[
 
 @app.route('/books')
 def books():
-    return jsonify(book)
+    return jsonify(booklist)
 
 @app.route('/book/<int:id>')
 def book(id):
@@ -56,6 +58,62 @@ def book(id):
         return jsonify({'message':'Book not found'}) ,404
     return jsonify(rt_book)
 
+@app.route('/books',methods=["POST"])
+def create_book():
+    if not request.json:
+        abort()
+    
+    book={
+        'id': booklist[-1]['id']+1,
+        'title': request.json.get('title'),
+        'author':request.json.get('author'),
+    }
+    booklist.append(book)
+    return jsonify({'book':book}), 201
 
+@app.route('/book/<int:id>',methods=['PUT'])
+def update_book(id):
+    book =[book for book in booklist if book['id'] == id]
+    if len(book) == 0:
+        abort(404)
+    if 'title' in request.json and type(request.json.get('title'))!= str:
+        abort(400)
+    if 'author' in request.json and type(request.json.get('author')) != str:
+        abort(400)
+    book[0]['title'] = request.json.get('title')
+    book[0]['author'] = request.json.get('author')
+
+    return jsonify(book)
+
+@app.route('/book/<int:id>', methods=["DELETE"])
+def delete_book(id):
+    book =[book for book in booklist if book['id'] == id]
+    if len(book) == 0:
+        abort(404)
+    booklist.remove(book[0])
+    return jsonify({"status":"deleted"})
+
+
+#install a module called requests to send the API request
+#using the command pip install requests
+#defining the API url
+API_URL = ('https://api.genderize.io/?name={}')
+
+#createw a new function for sending the api requeset to the url
+def send_api(name):
+    print(API_URL)
+    try:
+        data = requests.get(API_URL.format(name)).json()
+    except Exception as e: 
+        print(e)
+        data =None
+    return data
+
+
+@app.route('/gender/<name>')
+def gender(name):
+    response = send_api(name)
+    rt =response["name"]+" is "+response["gender"]
+    return rt
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
