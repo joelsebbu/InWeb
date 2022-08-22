d9088542129a1a7e3ecc68f92d29c2bc08aaf5e2
joelsebbu <joel18sebastian@gmail.com>
2022-08-22 13:43:53 +0530

    now it works

--- BackEnd/flask_001/app.py
+++ BackEnd/flask_001/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,redirect,url_for,request
+from flask import Flask,redirect,url_for,request,jsonify,abort
 
 app= Flask(__name__)
 
@@ -20,11 +20,41 @@ def login():
     email = request.args.get('email')
     password = request.args.get('password')
     return f'<h1>Username: {username}, Email: {email}, Password: {password}</h1>'
-
+    #POST
     # username =request.form.get('name') 
     # email = request.form.get('email')
     # password = request.form.get('password')
     # return f'<h1>Hello, {username}!</h1><h2>{email}</h2><h3>{password}</h3>'
+
+
+booklist=[
+    {
+        'id':1,
+        'title':'Harry Potter',
+        'author':'J.K. Rowling',
+    },
+    {
+        'id':2,
+        'title':'The Alchemist',
+        'author':'Paulo Coelho',
+    },
+    {
+        'id':3,
+        'title':'Jungle Book',
+        'author':'R. Kipling',
+    }
+]  
+
+@app.route('/books')
+def books():
+    return jsonify(book)
+
+@app.route('/book/<int:id>')
+def book(id):
+    rt_book =list(filter(lambda t: t['id']==id,booklist))
+    if len(rt_book)==0:
+        return jsonify({'message':'Book not found'}) ,404
+    return jsonify(rt_book)
 
 
 if __name__ == '__main__':
