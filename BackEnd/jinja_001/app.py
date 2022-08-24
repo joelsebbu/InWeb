8105d9045d699f3b575494aeabac40680067ca9c
joelsebbu <joel18sebastian@gmail.com>
2022-08-24 10:10:30 +0530

    for the project top and side set

--- BackEnd/jinja_001/app.py
+++ BackEnd/jinja_001/app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template
-from flask_material import Material
+
 app = Flask(__name__)
 # app.config['SECRETE']
-Material(app)
 
 
 @app.route('/user/<string:name>')
 def user(name):
-    return render_template('user.html',username = name, n= 5)
+    return render_template('user.html')
 
 @app.route('/material/<string:name>')
 def material(name):
